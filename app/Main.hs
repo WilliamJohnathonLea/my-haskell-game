914c5fe93f1a01d4d75257cf90b45fad5cebc94f
William Lea <william.lea@digital.hmrc.gov.uk>
2017-04-01 15:26:24 +0100

    Fixed print out

--- app/Main.hs
+++ app/Main.hs
@@ -2,22 +2,18 @@ module Main where
 
 import Character
 
-characterCreation :: IO ()
-characterCreation = do
-  putStrLn "What will you call them?"
-  name <- getLine
-  putStrLn "What are their attributes?"
-  putStr "Strength: "
-  str <- getLine
-  putStr "Intelligence: "
-  intel <- getLine
-  putStr "Stamina: "
-  stam <- getLine
-  putStrLn (show (createCharacter name 100 (createStats (read str::Int) (read intel::Int) (read stam::Int))))
-  return ()
-
 main :: IO ()
 main = do
   putStrLn "Welcome to The End of the World!"
   putStrLn "Please create your character\n"
-  characterCreation
+  putStrLn "What will you call them?"
+  name <- getLine
+  putStrLn "What are their attributes?"
+  putStrLn "Strength: "
+  str <- getLine
+  putStrLn "Intelligence: "
+  intel <- getLine
+  putStrLn "Stamina: "
+  stam <- getLine
+  putStrLn (show (createCharacter name 100 (createStats (read str::Int) (read intel::Int) (read stam::Int))))
+  return ()
