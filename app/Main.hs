e9d4cdda5ed296c71af76f06f368ee3b10508dac
William Lea <william.lea@digital.hmrc.gov.uk>
2017-04-09 13:43:27 +0100

    Building on the main game flow

--- app/Main.hs
+++ app/Main.hs
@@ -4,18 +4,33 @@ import Game
 import Character
 import GameMap
 
-main :: IO ()
-main = do
+intro :: IO ()
+intro = do
   putStrLn "Welcome to The End of the World!"
   putStrLn "Please create your character\n"
+  return ()
+
+characterCreation :: IO Character
+characterCreation = do
   putStrLn "What will you call them?"
   name <- getLine
   putStrLn "What are their attributes?"
-  putStrLn "Strength: "
+  putStrLn "Strength:"
   str <- getLine
-  putStrLn "Intelligence: "
+  putStrLn "Intelligence:"
   intel <- getLine
-  putStrLn "Stamina: "
+  putStrLn "Stamina:"
   stam <- getLine
-  putStrLn (show (createCharacter name 100 (createStats (read str::Int) (read intel::Int) (read stam::Int))))
+  return (createCharacter name 100 (createStats (read str::Int) (read intel::Int) (read stam::Int)))
+
+gameCreation :: Character -> IO Game
+gameCreation c = do
+  putStrLn "The adventure begins..."
+  return (newGame c (generateMap 10 10))
+
+main :: IO ()
+main = do
+  intro
+  character <- characterCreation
+  gameCreation character
   return ()
